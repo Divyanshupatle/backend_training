5c89789739174437f13b04c6d5135db0c186a42c
Divyanshu kumar <divyanshupatle123@gmail.com>
2022-08-06 00:17:01 +0530

    logger

---
+++ src/logger/logger.js
@@ -0,0 +1,8 @@
+const batchName = "Plutonium"
+
+let welcome = function() {
+    console.log('‘Welcome to my application. I am divyanshu patle and a part of FunctionUp Plutonium cohort.')
+}
+
+module.exports.name = batchName
+module.exports.welcome = welcome
