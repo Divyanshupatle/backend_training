1e6c1418564eadcf9737aff24e80473d0150c8ff
Divyanshu kumar <divyanshupatle123@gmail.com>
2022-08-11 13:28:00 +0530

    players

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,141 @@
 const express = require('express');
 const router = express.Router();
+//Your player collection should be an ARRAY of player objects. Each player object should have the following attributes:
+let players =
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ]
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ]
+        },
+    ]
+//Write a POST /players api that creates a new player ( i.e. that saves a player’s details and doesn’t allow saving the data of a player with a name that already exists in the data)
 
-router.get('/students/:name', function(req, res) {
+router.post('/players', function (req, res) {
+    //LOGIC WILL COME HERE
+    let new_players= req.body
+    for (let i = 0; i < players.length; i++) {
+        let find = players[i]
+        // find is veriable 
+        if (find.name == new_players.name){
+            // use equality operator
+            return res.send("the name is exisgting");
+        // return is terminat the function
+    }}
+    players.push(new_players)
+    //add object in players variable
+            res.send(players) 
+})
+
+
+
+
+// console.log(auth ? "yes":"no")
+
+
+
+router.get("/post-query", function (req, res) {
+    let marks=req.query.marks
+    let result=marks>40 ? "pass":"fail"
+    res.send({data:data,status:true})
+})
+
+
+
+
+
+router.get('/students/:name', function (req, res) {
     let studentName = req.params.name
     console.log(studentName)
     res.send(studentName)
 })
 
-router.get("/random" , function(req, res) {
+router.get("/random", function (req, res) {
     res.send("hi there")
 })
 
 
-router.get("/test-api" , function(req, res) {
+router.get("/test-api", function (req, res) {
     res.send("hi FunctionUp")
 })
 
 
-router.get("/test-api-2" , function(req, res) {
+router.get("/test-api-2", function (req, res) {
     res.send("hi FunctionUp. This is another cool API")
 })
 
 
-router.get("/test-api-3" , function(req, res) {
+router.get("/test-api-3", function (req, res) {
     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
 })
 
 
-router.get("/test-api-4" , function(req, res) {
+router.get("/test-api-4", function (req, res) {
     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
 })
 
 
 
-router.get("/test-api-5" , function(req, res) {
+router.get("/test-api-5", function (req, res) {
     res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
 })
 
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
+router.get("/test-api-6", function (req, res) {
+    res.send({ a: 56, b: 45 })
 })
 
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
+router.post("/test-post", function (req, res) {
+    let arr=[1,4,7,9,54,7,8,77,6,672,745,,66,65,555,55]
+    input=req.query;
+    let new_arr=(a=>a>input);
+
 })
 
 
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
+router.post("/test-post-2", function (req, res) {
+    res.send({ msg: "hi", status: true })
 })
 
-router.post("/test-post-3", function(req, res) {
+router.post("/test-post-3", function (req, res) {
     // let id = req.body.user
     // let pwd= req.body.password
 
     // console.log( id , pwd)
 
-    console.log( req.body )
+    console.log(req.body)
 
-    res.send(  { msg: "hi" , status: true }  )
+    res.send({ msg: "hi", status: true })
 })
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
+router.post("/test-post-4", function (req, res) {
+    let arr = [12, "functionup"]
+    let ele = req.body.element
     arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
+    res.send({ msg: arr, status: true })
 })
 
 module.exports = router;
