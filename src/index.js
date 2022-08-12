0789bd1ae1923674a06a0951ffb19665c462a300
Divyanshu kumar <divyanshupatle123@gmail.com>
2022-08-12 16:10:01 +0530

    mongo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
+mongoose.connect("mongodb+srv://team-28-plutonium:J9CHWny8TKmvChdL@cluster0.zrvtnuy.mongodb.net/team-28-plutonium-DB?retryWrites=true&w=majority",{
+    userNewUrlParser: true
+}).then( () => console.log("mongoDb is connected"))
+.catch(err => console.log(err))
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
