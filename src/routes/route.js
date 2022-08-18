01e9a1ca7bef9633bc3ca97b4172a971858f11e2
Divyanshu kumar <divyanshupatle123@gmail.com>
2022-08-18 15:04:47 +0530

    pending assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -16,5 +16,39 @@ router.get("/getUsersData", UserController.getUsersData)
 router.post("/createbook",UserController.createbook)
 router.get("/getbooks", UserController.getbooks)
 
+let person =[
+    {
+        name: "pk",
+        age: 10,
+        votingstatus: false
+    },
+    {
+        name: "sk",
+        age: 20,
+        votingstatus: false
+    },
+    {
+        name: "sp",
+        age: 40,
+        votingstatus: false
+    },
+    {
+        name: "ho",
+        age: 70,
+        votingstatus: false
+    },
+    {
+        name: "oo",
+        age: 5,
+        votingstatus: false
+    },
+]
+router.post("/voting", function (req, res) {
+    inputAge=req.query.votingAge;
+    person.map(per=>(per.age>inputAge)?per.votingstatus=true:per.votingstatus);
+    let final=person.filter(pson=>{if(pson.votingstatus==true){return pson.name}});
+    res.send({data:final});
+});
+
 
 module.exports = router;
