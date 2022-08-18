73ee08a8e49345c828fafc1d8ee89d3e296bd4f5
Divyanshu kumar <divyanshupatle123@gmail.com>
2022-08-18 18:43:35 +0530

    fr

--- src/models/bookmodel1.js
+++ src/models/bookmodel1.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
-    Books:{
+    
         name: String,
    author_id: Number,
        price: Number,
      ratings: Number
-    }
+
 })
 module.exports= mongoose.model('Book',bookSchema)
 
 
 const authorSchema = new mongoose.Schema({
-    author:{
+    
    authorId:Number,
  authorName: String,
          age: Number,
      address: String,
      
-    }
 },{ timestamps: true })
 
 module.exports= mongoose.model('Author',authorSchema)
